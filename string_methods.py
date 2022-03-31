2f7178688373b92d12ff772b2e7e4ec58a7a3d28
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-03-31 11:03:49 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,30 @@
+# n='shainaz'
+# print(n.capitalize())
+# print(n.upper())
+# print(n.isupper())
+# a='SHAINAZ'
+# print(a.lower())
+# print(a.casefold())
+# print(a.islower())
+
+# print(n.center(6))
+# print(n.center(30,"*"))
+# print(n.center(30))
+# b="thank you"
+# print(b.title())
+# print(b.istitle())
+# C="thaNk yoU"
+# print(C.title())
+# print(C.istitle())
+
+# print(n,type(n))
+# print(n.encode())
+# w=n.encode()
+# print(w)
+# print(w.decode())
+
+# p=b'shainaz'
+# print(p.decode())
+# s=p.decode()
+# print(s)
+# print(s.encode())
